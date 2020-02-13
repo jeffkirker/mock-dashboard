d501c2a5954a3bbf30caf5c7c4244746265e5648
jkirker <jkirker@ualberta.ca>
2020-02-12 23:47:11 -0700

    removed unused chart.js component

--- src/components/SummaryDashboard/SummaryStats/SummaryStats.js
+++ src/components/SummaryDashboard/SummaryStats/SummaryStats.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import Chart from '../../Chart';
 import Paper from '@material-ui/core/Paper';
 import classes from './SummaryStats.css'
 import Deposits from '../../Deposits';
